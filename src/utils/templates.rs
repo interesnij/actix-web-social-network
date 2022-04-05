83bf2382a0113201f2053b81931dcf2bb88597b1
interesnij <ochkarik1983@mail.ru>
2022-04-05 20:49:40 +0300

    J

--- src/utils/templates.rs
+++ src/utils/templates.rs
@@ -1,10 +1,11 @@
 use actix_web::HttpRequest;
 use actix_web_httpauth::headers::authorization::{Authorization, Basic};
+use actix_web::http::header::Header;
 
 pub fn get_default_template(req: HttpRequest) -> (String, bool) {
     // получаем папку шаблона и проверяем на хост-админа
     let mut _type = "".to_string();
-    let auth = Authorization::<Basic>::parse(req);
+    let auth = Authorization::<Basic>::parse(&req);
     for header in req.headers().into_iter() {
         if header.0 == "user-agent" {
             let _val = format!("{:?}", header.1);
