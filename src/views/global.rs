9298f387e140e9e3b9fe9405430f5398bc79f09c
interesnij <ochkarik1983@mail.ru>
2022-04-08 12:32:02 +0300

    J

--- src/views/global.rs
+++ src/views/global.rs
@@ -40,7 +40,7 @@ pub async fn phone_send(req: HttpRequest, _phone: web::Path<String>) -> impl Res
     let connection = establish_connection();
     let mut data = Context::new();
     let mut a = Vec::new();
-    let _url = "https://api.ucaller.ru/v1.0/initCall?service_id=12203&key=GhfrKn0XKAmA1oVnyEzOnMI5uBnFN4ck&phone=" + _phone.to_string();
+    let _url = "https://api.ucaller.ru/v1.0/initCall?service_id=12203&key=GhfrKn0XKAmA1oVnyEzOnMI5uBnFN4ck&phone=".to_owned() + _phone.to_string();
     request::get(_url, &mut a);
     let answer = io::stdout().write(&a);
     println!("{:?}", &answer);
