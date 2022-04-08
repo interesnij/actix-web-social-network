704d5058c23f1ce2c239e95ca875e92367207f33
interesnij <ochkarik1983@mail.ru>
2022-04-08 16:18:58 +0300

    J

--- src/views/global.rs
+++ src/views/global.rs
@@ -58,8 +58,10 @@ pub async fn phone_send(req: HttpRequest, _phone: web::Path<String>) -> impl Res
     let (_type, _is_host_admin) = get_default_template(req);
     let mut data = Context::new();
     let _url = "https://api.ucaller.ru/v1.0/initCall?service_id=12203&key=GhfrKn0XKAmA1oVnyEzOnMI5uBnFN4ck&phone=".to_owned() + &_phone.to_string();
-    let __request = reqwest::get(_url)?
-        .text()?;
+    let __request = reqwest::get(_url)
+        .await
+        .text()
+        .await;
     println!("request - {:?}", &__request);
 
     let _template = _type + &"main/auth/phone_verification.html".to_string();
