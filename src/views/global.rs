beb11af3ef185c9b9d08ac00b11ef3518401a344
interesnij <ochkarik1983@mail.ru>
2022-04-08 14:43:01 +0300

    J

--- src/views/global.rs
+++ src/views/global.rs
@@ -65,8 +65,9 @@ pub async fn phone_send(req: HttpRequest, _phone: web::Path<String>) -> impl Res
     let json_answer = io::stdout()
         .write(&a)
         .ok()
-        .expect("E");
-    //let answer: PhoneJson = serde_json::from_str(&json_answer).unwrap();
+        .expect("E")
+        .to_string();
+    let answer: PhoneJson = serde_json::from_str(&json_answer).unwrap();
     println!("{:?}", &json_answer);
     //data.insert("phone", &answer);
 
