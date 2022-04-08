754c5099f9f47fe62b04b2307351bb29dc1fc04f
interesnij <ochkarik1983@mail.ru>
2022-04-08 14:12:41 +0300

    J

--- src/views/global.rs
+++ src/views/global.rs
@@ -63,11 +63,11 @@ pub async fn phone_send(req: HttpRequest, _phone: web::Path<String>) -> impl Res
     let _url = "https://api.ucaller.ru/v1.0/initCall?service_id=12203&key=GhfrKn0XKAmA1oVnyEzOnMI5uBnFN4ck&phone=".to_owned() + &_phone.to_string();
     request::get(_url, &mut a);
     let answer: PhoneJson = io::stdout()
-        .write(&a)
-        .expect("E");
+        .write(&a);
+        //.expect("E");
 
     //println!("{:?}", json.find_path(&["phone"]).unwrap());
-    println!("{:?}", &answer); 
+    println!("{:?}", &answer);
     //data.insert("phone", &answer);
 
     let _template = _type + &"main/auth/phone_verification.html".to_string();
