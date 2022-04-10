411d84850dbfe6ea1c93fc50d85b102b4a388514
interesnij <ochkarik1983@mail.ru>
2022-04-10 10:23:53 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod views;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::middleware::cors::Cors;
+    use actix_cors::Cors;
     use actix_files::Files;
     use crate::routes::routes;
     use actix_redis::RedisSession;
@@ -24,16 +24,14 @@ async fn main() -> std::io::Result<()> {
         let media_files = Files::new("/media", "media/").show_files_listing();
         //let cors = Cors::default();
         let private_key = actix_web::cookie::Key::generate();
-
+        let cors = Cors::default()
+            .allowed_origin("151.248.120.138:9015")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600);
+            
         App::new()
-            .configure(|app| Cors::for_app(app) // <- Construct CORS middleware builder
-                .allowed_origin("151.248.120.138:9015")
-                .allowed_methods(vec!["GET", "POST"])
-                .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                .allowed_header(header::CONTENT_TYPE)
-                .max_age(3600)
-                .register()
-            )
             .wrap(RedisSession::new("127.0.0.1:6379", private_key.master()))
             .service(static_files)
             .service(media_files)
