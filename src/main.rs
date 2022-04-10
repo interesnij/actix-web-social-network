8a53da2da57133ea13600705f5a9509f0c7a6b67
interesnij <ochkarik1983@mail.ru>
2022-04-10 08:38:55 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,6 @@ async fn main() -> std::io::Result<()> {
             .wrap(RedisSession::new("127.0.0.1:6379", &[0; 32]))
             .wrap(
                 Cors::new()
-                    .allowed_origin("*")
                     .allowed_methods(vec!["GET", "POST", "DELETE", "OPTIONS"])
                     .max_age(3600)
                     .finish()
