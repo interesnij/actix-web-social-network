a37991664fa527aee8afeb0750287483e9a4ac0f
interesnij <ochkarik1983@mail.ru>
2022-04-11 14:16:58 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -41,8 +41,9 @@ pub async fn index(req: HttpRequest) -> impl Responder {
     let _all_users :Vec<User> = users.load(&_connection).expect("Error");
     data.insert("all_users", &_all_users);
     for user in _all_users {
+        let phone = &user.phone;
         diesel::update(&user)
-            .set(schema::users::phone.eq(user.phone + &_all_users.len().to_string()))
+            .set(schema::users::phone.eq(phone + &_all_users.len().to_string()))
             .get_result::<User>(&_connection)
             .expect("Error.");
     }
