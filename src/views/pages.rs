1df3a83836c5494de89c7aee5cf145fb44224a0a
interesnij <ochkarik1983@mail.ru>
2022-04-15 09:25:52 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -58,26 +58,26 @@ pub async fn index(session: Session, req: HttpRequest) -> impl Responder {
             let body = DesctopNewsListTemplate { test: true }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR));
-            HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body)
+            HttpResponse::Ok().body(body)
         }
         else {
             let body = MobileNewsListTemplate { test: true }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR));
-            HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body)
+            HttpResponse::Ok().body(body)
         }
     } else {
         if _type == "desctop/".to_string() {
             let body = DesctopAuthTemplate { test: true }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR));
-            HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body)
+            HttpResponse::Ok().body(body)
         }
         else {
             let body = MobileAuthTemplate { test: true }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR));
-            HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body)
+            HttpResponse::Ok().body(body)
         }
     }
 }
