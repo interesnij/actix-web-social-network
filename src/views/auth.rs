ed8ede47406c90675d0e8b554e0d66ffff851d52
interesnij <ochkarik1983@mail.ru>
2022-04-15 13:09:30 +0300

    J

--- src/views/auth.rs
+++ src/views/auth.rs
@@ -25,7 +25,6 @@ use futures_util::stream::StreamExt as _;
 
 
 pub fn auth_routes(config: &mut web::ServiceConfig) {
-    config.route("/phone_window/", web::get().to(phone_window));
     config.route("/phone_send/{phone}/", web::get().to(phone_send));
     config.route("/phone_verify/{phone}/{code}/", web::get().to(phone_verify));
     config.route("/signup/", web::get().to(process_signup));
@@ -773,34 +772,6 @@ pub async fn process_signup(session: Session, req: HttpRequest) -> impl Responde
         set_current_user(&session, &_session_user);
     }
     HttpResponse::Ok().body(format!("ok"))
-}
-
-pub async fn phone_window() -> impl Responder {
-    //let (_type, _is_host_admin) = get_default_template(req);
-    //let mut data = Context::new();
-    let _template = "<form class='h-sm-auto final_process_form'>
-      <h1 class='font-weight-light mb-3 mt-4 text-center user_name'></h1>
-      <p class='text-center'>
-        Вам нужно подтвердить с помощью телефона свой профиль, и в будущем использовать номер телефона
-        в качестве логина к профилю соцсети трезвый.рус.
-      </p>
-      <div class='row'>
-        <div class='col-md-2'></div>
-        <div class='col-md-4' style='display: inline-flex;margin-top: 12px;margin-bottom: 12px;'>
-          <input type='number' id='phone' min='0' onkeyup='phone_check();' class='form-control border-0' placeholder='Телефон'>
-          <hr class='my-0'>
-        </div>
-        <div class='col-md-4' style='margin-top: 12px;'>
-          <button type='button' disabled='disabled' id='phone_send' class='btn btn-primary pink-gradient'>Получить код</button>
-        </div>
-        <div class='col-md-2'></div>
-      </div>
-      <div id='jsondata' style='margin-top:50px'></div>
-
-      <div id='jsondata2' style='margin-top:50px'></div>
-    </form>";
-    //let rendered = TEMPLATES.render(&_template, &data).unwrap();
-    HttpResponse::Ok().body(_template)
 }
 
 #[derive(Deserialize, Debug)]
