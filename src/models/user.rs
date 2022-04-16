3f8422520cbb434ef47b819b0bbd2e28598d113a
interesnij <ochkarik1983@mail.ru>
2022-04-16 13:54:54 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -325,7 +325,7 @@ impl User {
         use crate::schema::user_locations::dsl::user_locations;
 
         let _connection = establish_connection();
-        all_user_locations = user_locations
+        let all_user_locations = user_locations
             .filter(schema::user_locations::user_id.eq(self.id))
             .order(schema::user_locations::id.desc())
             .load::<UserLocation>(&_connection)
