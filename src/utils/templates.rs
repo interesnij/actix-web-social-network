41d9efede294d53dc851610acbda35dfbac2c6f6
interesnij <ochkarik1983@mail.ru>
2022-04-16 07:40:02 +0300

    J

--- src/utils/templates.rs
+++ src/utils/templates.rs
@@ -4,6 +4,7 @@ use crate::utils::establish_connection;
 use crate::schema;
 use diesel::prelude::*;
 use crate::errors::AuthError;
+use crate::models::User;
 
 
 pub fn get_folder(req: HttpRequest) -> String {
@@ -19,6 +20,44 @@ pub fn get_folder(req: HttpRequest) -> String {
         }
     };
     _type
+}
+
+pub fn get_request_user_data_2(session: Session) -> (User, String) {
+    use crate::models::SessionUser;
+
+    let _connection = establish_connection();
+    //let _request_user = get_current_user(&session);
+    let mut user_id = 0;
+    let mut have_link = "".to_string();
+    let mut s_avatar = "".to_string();
+    if let Some(user_str) = session.get::<String>("user")
+        .map_err(|_| AuthError::AuthenticationError(String::from("Не удалось извлечь пользователя из сеанса")))
+        .unwrap() {
+            let user: SessionUser = serde_json::from_str(&user_str).expect("E.");
+            user_id = user.id;
+        }
+    if user_id != 0 {
+        use crate::schema::{
+            design_settings::dsl::design_settings,
+            users::dsl::users,
+        };
+        use crate::models::{
+            DesignSetting,
+            User
+        };
+        let _user = &users
+            .filter(schema::users::id.eq(&user_id))
+            .load::<User>(&_connection)
+            .expect("E")[0];
+        let _design = design_settings
+            .filter(schema::design_settings::user_id.eq(&user_id))
+            .load::<DesignSetting>(&_connection)
+            .expect("E");
+        let background = &_design[0].background;
+        (_user, background.to_string())
+    } else {
+        (User, "".to_string())
+    }
 }
 
 pub fn get_request_user_data(session: Session) -> (
