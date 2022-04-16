16572ffda3f98836d560471e8cb7b3c366261060
interesnij <ochkarik1983@mail.ru>
2022-04-16 08:58:48 +0300

    J

--- src/utils/templates.rs
+++ src/utils/templates.rs
@@ -21,10 +21,10 @@ pub fn get_folder(req: HttpRequest) -> String {
     };
     _type
 }
-//&'static
-pub fn get_request_user_data_2(session: Session) -> (User, String) {
+
+pub fn get_request_user_data(session: Session) -> (Vec<User>, String) {
     use crate::models::SessionUser;
-    use crate::schema::{
+    use crate::schema::{ 
         design_settings::dsl::design_settings,
         users::dsl::users,
     };
@@ -44,74 +44,14 @@ pub fn get_request_user_data_2(session: Session) -> (User, String) {
             .load::<DesignSetting>(&_connection)
             .expect("E");
         let background = &_design[0].background;
-        let _users = users
+        (users
             .filter(schema::users::id.eq(user_id))
             .load::<User>(&_connection)
-            .expect("E");
-        let req_user = _users[0];
-        (req_user, background.to_string())
+            .expect("E"), background.to_string())
     } else {
-        let _users = users
+        (users
             .filter(schema::users::id.eq(1))
             .load::<User>(&_connection)
-            .expect("E");
-        let req_user = _users[0];
-        (req_user, "".to_string())
-    }
-}
-
-pub fn get_request_user_data(session: Session) -> (
-        i32, String, i16, String, String, i16, String, String, String
-    ) {
-    use crate::models::SessionUser;
-
-    let _connection = establish_connection();
-    //let _request_user = get_current_user(&session);
-    let mut user_id = 0;
-    let mut have_link = "".to_string();
-    let mut s_avatar = "".to_string();
-    if let Some(user_str) = session.get::<String>("user")
-        .map_err(|_| AuthError::AuthenticationError(String::from("Не удалось извлечь пользователя из сеанса")))
-        .unwrap() {
-            let user: SessionUser = serde_json::from_str(&user_str).expect("E.");
-            user_id = user.id;
-        }
-    if user_id != 0 {
-        use crate::schema::{
-            design_settings::dsl::design_settings,
-            users::dsl::users,
-        };
-        use crate::models::{
-            DesignSetting,
-            User
-        };
-        let _user = &users
-            .filter(schema::users::id.eq(&user_id))
-            .load::<User>(&_connection)
-            .expect("E")[0];
-        let _design = design_settings
-            .filter(schema::design_settings::user_id.eq(&user_id))
-            .load::<DesignSetting>(&_connection)
-            .expect("E");
-        let background = &_design[0].background;
-        if _user.have_link.is_some() {
-            have_link = _user.have_link.as_ref().expect("E.").to_string()
-        }
-        if _user.s_avatar.is_some() {
-            s_avatar = _user.s_avatar.as_ref().expect("E.").to_string()
-        }
-        (
-            user_id,
-            _user.get_full_name(),
-            _user.types.clone(),
-            _user.gender.clone(),
-            _user.language.clone(),
-            _user.perm.clone(),
-            have_link,
-            s_avatar,
-            background.to_string(),
-        )
-    } else {
-        (0, "".to_string(), 0, "".to_string(), "".to_string(), 0, "".to_string(), "".to_string(), "".to_string())
+            .expect("E"), "".to_string())
     }
 }
--- src/views/pages.rs
+++ src/views/pages.rs
@@ -34,16 +34,9 @@ struct DesctopAuthTemplate {
 #[derive(TemplateOnce)]
 #[template(path = "desctop/main/lists/news_list.stpl")]
 struct DesctopNewsListTemplate {
-    title:      String,
-    request_id: i32,
-    fio:        String,
-    types:      i16,
-    gender:     String,
-    language:   String,
-    perm:       i16,
-    have_link:  String,
-    s_avatar:   String,
-    background: String,
+    title:        String,
+    request_user: User,
+    background:   String,
 }
 
 #[derive(TemplateOnce)]
@@ -54,16 +47,9 @@ struct MobileAuthTemplate {
 #[derive(TemplateOnce)]
 #[template(path = "mobile/main/lists/news_list.stpl")]
 struct MobileNewsListTemplate {
-    title:      String,
-    request_id: i32,
-    fio:        String,
-    types:      i16,
-    gender:     String,
-    language:   String,
-    perm:       i16,
-    have_link:  String,
-    s_avatar:   String,
-    background: String,
+    title:        String,
+    request_user: User,
+    background:   String,
 }
 
 pub async fn index(session: Session, req: HttpRequest) -> actix_web::Result<HttpResponse> {
@@ -71,20 +57,13 @@ pub async fn index(session: Session, req: HttpRequest) -> actix_web::Result<Http
     let _type = get_folder(req);
     if is_signed_in(&session) {
 
-        let (_request_id, _fio, _types, _gender, _language, _perm, _have_link, _s_avatar, _background) = get_request_user_data(session);
+        let (_request_user, _background) = get_request_user_data(session);
 
         if _type == "desctop/".to_string() {
             let body = DesctopNewsListTemplate {
-                title:      "Новости".to_string(),
-                request_id: _request_id,
-                fio:        _fio,
-                types:      _types,
-                gender:     _gender,
-                language:   _language,
-                perm:       _perm,
-                have_link:  _have_link,
-                s_avatar:   _s_avatar,
-                background: _background,
+                title:        "Новости".to_string(),
+                request_user: _request_user,
+                background:   _background,
             }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
@@ -94,16 +73,9 @@ pub async fn index(session: Session, req: HttpRequest) -> actix_web::Result<Http
         }
         else {
             let body = MobileNewsListTemplate {
-                title:      "Новости".to_string(),
-                request_id: _request_id,
-                fio:        _fio,
-                types:      _types,
-                gender:     _gender,
-                language:   _language,
-                perm:       _perm,
-                have_link:  _have_link,
-                s_avatar:   _s_avatar,
-                background: _background,
+                title:        "Новости".to_string(),
+                request_user: _request_user,
+                background:   _background,
             }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
@@ -136,49 +108,28 @@ pub async fn index(session: Session, req: HttpRequest) -> actix_web::Result<Http
 #[derive(TemplateOnce)]
 #[template(path = "desctop/main/lists/featured_list.stpl")]
 struct DesctopFeaturedListTemplate {
-    title:      String,
-    request_id: i32,
-    fio:        String,
-    types:      i16,
-    gender:     String,
-    language:   String,
-    perm:       i16,
-    have_link:  String,
-    s_avatar:   String,
-    background: String,
+    title:        String,
+    request_user: User,
+    background:   String,
 }
 #[derive(TemplateOnce)]
 #[template(path = "mobile/main/lists/featured_list.stpl")]
 struct MobileFeaturedListTemplate {
-    title:      String,
-    request_id: i32,
-    fio:        String,
-    types:      i16,
-    gender:     String,
-    language:   String,
-    perm:       i16,
-    have_link:  String,
-    s_avatar:   String,
-    background: String,
+    title:        String,
+    request_user: User,
+    background:   String,
 }
 pub async fn featured_list(session: Session, req: HttpRequest) -> actix_web::Result<HttpResponse> {
     let _connection = establish_connection();
     let _type = get_folder(req);
     if is_signed_in(&session) {
-        let (_request_id, _fio, _types, _gender, _language, _perm, _have_link, _s_avatar, _background) = get_request_user_data(session);
+        let (_request_user, _background) = get_request_user_data(session);
 
         if _type == "desctop/".to_string() {
             let body = DesctopFeaturedListTemplate {
                 title:      "Рекомендации".to_string(),
-                request_id: _request_id,
-                fio:        _fio,
-                types:      _types,
-                gender:     _gender,
-                language:   _language,
-                perm:       _perm,
-                have_link:  _have_link,
-                s_avatar:   _s_avatar,
-                background: _background,
+                request_user: _request_user,
+                background:   _background,
             }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
@@ -189,15 +140,8 @@ pub async fn featured_list(session: Session, req: HttpRequest) -> actix_web::Res
         else {
             let body = MobileFeaturedListTemplate {
                 title:      "Рекомендации".to_string(),
-                request_id: _request_id,
-                fio:        _fio,
-                types:      _types,
-                gender:     _gender,
-                language:   _language,
-                perm:       _perm,
-                have_link:  _have_link,
-                s_avatar:   _s_avatar,
-                background: _background,
+                request_user: _request_user,
+                background:   _background,
             }
             .render_once()
             .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
