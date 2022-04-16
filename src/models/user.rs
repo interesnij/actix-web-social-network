cb4df642e12371ee88914ec8160af68a52bfe537
interesnij <ochkarik1983@mail.ru>
2022-04-16 18:49:21 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -616,7 +616,7 @@ impl User {
         };
         return stack;
     }
-    pub fn get_6_featured_friends_ids(&self) -> Vec<i32> {
+    pub fn get_6_featured_friends_ids(&self) -> Vec<&i32> {
         let mut stack = Vec::new();
         for (i, _item) in self.get_featured_friends_ids().iter().enumerate() {
             if i < 6 {
