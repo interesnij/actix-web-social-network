84d174f2d26eb0909ebc3ef62c8194d2953f46d6
interesnij <ochkarik1983@mail.ru>
2022-04-16 07:50:54 +0300

    J

--- src/utils/templates.rs
+++ src/utils/templates.rs
@@ -54,14 +54,14 @@ pub fn get_request_user_data_2(session: Session) -> (&'static User, String) {
             .load::<DesignSetting>(&_connection)
             .expect("E");
         let background = &_design[0].background;
-        (_user, background.to_string())
+        (&_user, background.to_string())
     } else {
         use crate::schema::users::dsl::users;
         let _user = &users
             .filter(schema::users::id.eq(1))
             .load::<User>(&_connection)
             .expect("E")[0];
-        (_user, "".to_string())
+        (&_user, "".to_string())
     }
 }
 
