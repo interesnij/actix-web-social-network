2c8b261dd98bbc5616a58cd22417e528870f7162
interesnij <ochkarik1983@mail.ru>
2022-04-16 14:04:01 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -330,7 +330,7 @@ impl User {
             .filter(schema::user_locations::user_id.eq(self.id))
             .order(schema::user_locations::id.desc())
             .load::<UserLocation>(&_connection)
-            .expect("E").into_iter().nth(0).as_deref().unwrap();
+            .expect("E").into_iter().nth(0).unwrap();
     }
 
 }
