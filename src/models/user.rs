6d1cee4f0eb5f45fd54671c707d13278f0219df7
interesnij <ochkarik1983@mail.ru>
2022-04-16 18:34:06 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -617,7 +617,7 @@ impl User {
         return stack;
     }
     pub fn get_6_featured_friends_ids(&self) -> Vec<i32> {
-        return self.get_featured_friends_ids()[0..6];
+        return self.get_featured_friends_ids().to_vec()[0..6];
     }
 }
 
