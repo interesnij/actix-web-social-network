959c19cd64e777d78b9dde45de4c19c806e9130e
interesnij <ochkarik1983@mail.ru>
2022-04-17 20:29:33 +0300

    J

--- src/models/friends.rs
+++ src/models/friends.rs
@@ -1,4 +1,4 @@
-use crate::schema::friends;
+use crate::schema::{friends, friends_perms};
 use diesel::{Queryable, Insertable};
 use serde::{Serialize, Deserialize};
 //use crate::utils::establish_connection;
@@ -19,4 +19,88 @@ pub struct NewFriend {
     pub user_id:           i32,
     pub target_user_id:    i32,
     pub visited:        i32,
+}
+
+
+/////// Friend //////
+#[derive(Debug, Queryable, Serialize, Identifiable)]
+pub struct FriendsPerm {
+    pub id:              i32,
+    pub user_id:         i32,
+    pub can_see_info:      Option<String>,
+    pub can_see_community: Option<String>,
+    pub can_see_friend:      Option<String>,
+    pub can_send_message: Option<String>,
+    pub can_add_in_chat:      Option<String>,
+    pub can_see_doc: Option<String>,
+    pub can_see_music:      Option<String>,
+    pub can_see_survey: Option<String>,
+    pub can_see_post:      Option<String>,
+    pub can_see_post_comment: Option<String>,
+    pub can_see_photo:      Option<String>,
+    pub can_see_photo_comment: Option<String>,
+    pub can_see_good:      Option<String>,
+    pub can_see_good_comment: Option<String>,
+    pub can_see_video:      Option<String>,
+    pub can_see_video_comment: Option<String>,
+    pub can_see_planner:      Option<String>,
+    pub can_see_planner_comment: Option<String>,
+    pub can_copy_post:      Option<String>,
+    pub can_copy_photo: Option<String>,
+    pub can_copy_good:      Option<String>,
+    pub can_copy_video: Option<String>,
+
+    pub can_copy_planner:      Option<String>,
+    pub can_copy_doc: Option<String>,
+    pub can_copy_music:      Option<String>,
+    pub can_copy_survey: Option<String>,
+    pub can_create_post:      Option<String>,
+    pub can_create_photo: Option<String>,
+    pub can_create_good:      Option<String>,
+    pub can_create_video: Option<String>,
+    pub can_create_planner:      Option<String>,
+    pub can_create_doc: Option<String>,
+    pub can_create_music:      Option<String>,
+    pub can_create_survey: Option<String>,
+
+}
+#[derive(Deserialize, Insertable)]
+#[table_name="friends_perms"]
+pub struct NewFriendsPerm {
+    pub user_id:         i32,
+    pub can_see_info:      Option<String>,
+    pub can_see_community: Option<String>,
+    pub can_see_friend:      Option<String>,
+    pub can_send_message: Option<String>,
+    pub can_add_in_chat:      Option<String>,
+    pub can_see_doc: Option<String>,
+    pub can_see_music:      Option<String>,
+    pub can_see_survey: Option<String>,
+    pub can_see_post:      Option<String>,
+    pub can_see_post_comment: Option<String>,
+    pub can_see_photo:      Option<String>,
+    pub can_see_photo_comment: Option<String>,
+    pub can_see_good:      Option<String>,
+    pub can_see_good_comment: Option<String>,
+    pub can_see_video:      Option<String>,
+    pub can_see_video_comment: Option<String>,
+    pub can_see_planner:      Option<String>,
+    pub can_see_planner_comment: Option<String>,
+    pub can_copy_post:      Option<String>,
+    pub can_copy_photo: Option<String>,
+    pub can_copy_good:      Option<String>,
+    pub can_copy_video: Option<String>,
+
+    pub can_copy_planner:      Option<String>,
+    pub can_copy_doc: Option<String>,
+    pub can_copy_music:      Option<String>,
+    pub can_copy_survey: Option<String>,
+    pub can_create_post:      Option<String>,
+    pub can_create_photo: Option<String>,
+    pub can_create_good:      Option<String>,
+    pub can_create_video: Option<String>,
+    pub can_create_planner:      Option<String>,
+    pub can_create_doc: Option<String>,
+    pub can_create_music:      Option<String>,
+    pub can_create_survey: Option<String>,
 }
