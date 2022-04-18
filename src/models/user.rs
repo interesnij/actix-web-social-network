dd9eea4ccd5a62208ae0e61936b7389de0e61813
interesnij <ochkarik1983@mail.ru>
2022-04-18 18:32:43 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -3669,7 +3669,7 @@ impl User {
         bool_stack.push(bool_can_see_survey);
         return bool_stack;
     }
-    pub fn set_friends_visible_perms(&self, action: String, users_ids: Vec<i8>, types: String) -> bool {
+    pub fn set_friends_visible_perms(&self, action: String, users_ids: Vec<i32>, types: String) -> bool {
         use crate::models::{Friend, FriendsVisiblePerm};
         use crate::schema::friends_visible_perms::dsl::friends_visible_perms;
         use crate::schema::friends::dsl::friends;
