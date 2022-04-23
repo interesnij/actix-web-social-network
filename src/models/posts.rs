f00336d506334999f9c574b9b0a1f9baa033b690
interesnij <ochkarik1983@mail.ru>
2022-04-23 17:26:10 +0300

    J

--- src/models/posts.rs
+++ src/models/posts.rs
@@ -1928,9 +1928,9 @@ impl Post {
     pub fn get_format_text(&self) -> String {
         use crate::utils::hide_text;
         if self.content.is_some() {
-            let unwrap = self.content.unwrap();
+            let unwrap = self.content.as_ref().unwrap();
             if unwrap.len() <= 101 {
-                return self.content.unwrap();
+                return self.content.as_ref().unwrap();
             }
             else {
                 let new_str = unwrap[..100].to_owned() + &"<br><a class='pointer show_post_text'>Показать полностью...</a><br><span style='display:none'>" + &unwrap[101..] + &"</span>";
