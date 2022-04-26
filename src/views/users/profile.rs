6077ea270df833f364d06ed8b096a72ae408cd3d
interesnij <ochkarik1983@mail.ru>
2022-04-26 11:34:42 +0300

    J

--- src/views/users/profile.rs
+++ src/views/users/profile.rs
@@ -25,7 +25,7 @@ pub async fn user_page(session: Session, req: HttpRequest) -> actix_web::Result<
         let _request_user = get_request_user_data(session);
 
         #[derive(TemplateOnce)]
-        #[template(path = format!("{}users/account/user.stpl", _type))]
+        #[template(path = "${_type}users/account/user.stpl")]
         struct UserPage {
             title:        String,
             request_user: User,
