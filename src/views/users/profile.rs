5b0c08778aa9d0bde78f077e857029dcca3cae19
interesnij <ochkarik1983@mail.ru>
2022-04-26 10:12:19 +0300

    J

--- src/views/users/profile.rs
+++ src/views/users/profile.rs
@@ -3,6 +3,7 @@ use actix_web::{
     HttpResponse,
     web,
     error::InternalError,
+    http::StatusCode,
 };
 use serde::Deserialize;
 use crate::utils::{is_signed_in, establish_connection, get_folder, get_request_user_data};
