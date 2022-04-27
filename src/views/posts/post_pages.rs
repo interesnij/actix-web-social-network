1e26cb7920b4bf2fbfb7975d0a164729ad5e3b8b
interesnij <ochkarik1983@mail.ru>
2022-04-27 14:38:03 +0300

    g

--- src/views/posts/post_pages.rs
+++ src/views/posts/post_pages.rs
@@ -65,10 +65,10 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
         if _type == "desctop/".to_string() {
             if list.community_id.is_some() {
 
-                let folder: &str = "desctop/";
-                let template: &str = "communities/lenta/list.stpl";
+                let folder: &'static str = "desctop/";
+                let template: &'static str = "communities/lenta/list.stpl";
                 #[derive(TemplateOnce)]
-                #[template(path = template)]
+                #[template(path = folder))]
                 struct UserPage {
                     list:         PostList,
                     request_user: User,
