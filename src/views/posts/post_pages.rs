e3764dbcb65180b7ac014abbd3674cf985ea7950
interesnij <ochkarik1983@mail.ru>
2022-04-27 13:51:41 +0300

    g

--- src/views/posts/post_pages.rs
+++ src/views/posts/post_pages.rs
@@ -73,7 +73,7 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
                     request_user: User,
                     is_page_list: bool,
                     is_user_can_see_post_list: bool,
-                    is_user_can_see_post_list: bool,
+                    is_user_can_create_posts: bool,
                 }
                 let body = UserPage {
                     title:        _user.get_full_name().clone(),
@@ -83,7 +83,7 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
                     user_profile: _profile,
                     is_my_user:   is_my_user,
                     is_user_can_see_post_list: is_user_can_see_post_list,
-                    is_user_can_see_post_list: is_user_can_see_post_list,
+                    is_user_can_create_posts: is_user_can_create_posts,
                 }
                 .render_once()
                 .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
@@ -98,7 +98,7 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
                     request_user: User,
                     is_page_list: bool,
                     is_user_can_see_post_list: bool,
-                    is_user_can_see_post_list: bool,
+                    is_user_can_create_posts: bool,
                 }
                 let body = UserPage {
                     title:        _user.get_full_name().clone(),
@@ -108,7 +108,7 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
                     user_profile: _profile,
                     is_my_user:   is_my_user,
                     is_user_can_see_post_list: is_user_can_see_post_list,
-                    is_user_can_see_post_list: is_user_can_see_post_list,
+                    is_user_can_create_posts: is_user_can_create_posts,
                 }
                 .render_once()
                 .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
@@ -120,14 +120,12 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
                 #[derive(TemplateOnce)]
                 #[template(path = "mobile/communties/lenta/list.stpl")]
                 struct UserPage {
-                    title:        _user.get_full_name().clone(),
-                    private_bools: _user.get_profile_all_can_see(*_request_user_id).clone(),
-                    request_user: _request_user,
-                    user:         _user,
-                    user_profile: _profile,
-                    is_my_user:   is_my_user,
-                    is_user_can_see_post_list: is_user_can_see_post_list,
-                    is_user_can_see_post_list: is_user_can_see_post_list,
+                    title:        String,
+                    list:         PostList,
+                    request_user: User,
+                    is_page_list: bool,
+                    is_user_can_see_post_list: bool,
+                    is_user_can_create_posts: bool,
                 }
                 let body = UserPage {
                     title:        _user.get_full_name().clone(),
@@ -145,14 +143,12 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
                 #[derive(TemplateOnce)]
                 #[template(path = "mobile/users/lenta/list.stpl")]
                 struct UserPage {
-                    title:        _user.get_full_name().clone(),
-                    private_bools: _user.get_profile_all_can_see(*_request_user_id).clone(),
-                    request_user: _request_user,
-                    user:         _user,
-                    user_profile: _profile,
-                    is_my_user:   is_my_user,
-                    is_user_can_see_post_list: is_user_can_see_post_list,
-                    is_user_can_see_post_list: is_user_can_see_post_list,
+                    title:        String,
+                    list:         PostList,
+                    request_user: User,
+                    is_page_list: bool,
+                    is_user_can_see_post_list: bool,
+                    is_user_can_create_posts: bool,
                 }
                 let body = UserPage {
                     title:        _user.get_full_name().clone(),
