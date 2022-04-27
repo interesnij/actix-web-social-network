f55b21f56bf9b0a2fb6bd77daf9fdd4835de4da7
interesnij <ochkarik1983@mail.ru>
2022-04-27 07:30:28 +0300

    g

--- src/models/user.rs
+++ src/models/user.rs
@@ -576,7 +576,7 @@ impl User {
             false => "&nbsp;<svg style='width: 17px;' class='svg_default' fill='currentColor' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z'/></svg>",
             _ => "",
         };
-        let gender = match self.is_man() {
+        let gender = match self.is_men() {
             true => "<i>Был ",
             false => "<i>Была ",
             _ => "",
