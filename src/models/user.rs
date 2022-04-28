12a8c65c73f9a62ffd165785f7de9e8fb2cd3ea5
interesnij <ochkarik1983@mail.ru>
2022-04-28 19:54:57 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -157,6 +157,14 @@ impl User {
             return "<svg fill='currentColor' class='svg_default svg_default_40' viewBox='0 0 24 24'><path d='M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z'/><path d='M0 0h24v24H0z' fill='none'/></svg>".to_string();
         }
     }
+    pub fn get_50_avatar(&self) -> String {
+        if self.s_avatar.is_some() {
+            return "<img style='border-radius:50px;width:50px;' alt='image' src='".to_owned() + &self.s_avatar.as_deref().unwrap().to_string() +  &"' />".to_string();
+        }
+        else {
+            return "<svg fill='currentColor' class='svg_default svg_default_50' viewBox='0 0 24 24'><path d='M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z'/><path d='M0 0h24v24H0z' fill='none'/></svg>".to_string();
+        }
+    }
 
     pub fn get_link(&self) -> String {
         if self.have_link.is_some() {
