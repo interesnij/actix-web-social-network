ec94423171ef88ba3b39bae03c7e0d98f9768b31
interesnij <ochkarik1983@mail.ru>
2022-04-30 12:25:41 +0300

    J

--- src/views/posts/post_pages.rs
+++ src/views/posts/post_pages.rs
@@ -33,61 +33,60 @@ pub fn post_routes(config: &mut web::ServiceConfig) {
 
 pub async fn add_user_post_list_page(session: Session, req: HttpRequest) -> actix_web::Result<HttpResponse> {
     if !is_signed_in(&session) {
-        Ok(HttpResponse::Ok()
+        Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""))
+    }
+    else {
+        let _request_user = get_request_user_data(session);
+        if list.user_id != _request_user.id {
+            Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""))
+        }
+        else {
+            #[derive(TemplateOnce)]
+            #[template(path = "desctop/posts/post_user/add_list.stpl")]
+            struct Template {
+                request_user: User,
+            }
+            let body = Template {
+                request_user: _request_user,
+            }
+            .render_once()
+            .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+            Ok(HttpResponse::Ok()
             .content_type("text/html; charset=utf-8")
-            .body(""))
+            .body(body))
+        }
     }
-
-    let _request_user = get_request_user_data(session);
-    if list.user_id != _request_user.id {
-        Ok(HttpResponse::Ok()
-            .content_type("text/html; charset=utf-8")
-            .body(""))
-    }
-    #[derive(TemplateOnce)]
-    #[template(path = "desctop/posts/post_user/add_list.stpl")]
-    struct Template {
-        request_user: User,
-    }
-    let body = Template {
-        request_user: _request_user,
-    }
-    .render_once()
-    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
-    Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(body))
 }
 pub async fn edit_user_post_list_page(session: Session, req: HttpRequest, _id: web::Path<i32>) -> actix_web::Result<HttpResponse> {
     if !is_signed_in(&session) {
-        Ok(HttpResponse::Ok()
-            .content_type("text/html; charset=utf-8")
-            .body(""))
+        Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""));
     }
-    let _request_user = get_request_user_data(session);
-    let _list_id : i32 = *_id;
-    let list = get_post_list(_list_id);
-    if list.user_id != _request_user.id {
-        Ok(HttpResponse::Ok()
-            .content_type("text/html; charset=utf-8")
-            .body(""))
-    }
-
-    #[derive(TemplateOnce)]
-    #[template(path = "desctop/posts/post_user/edit_list.stpl")]
-    struct YTemplate {
-        request_user: User,
-        list: PostList,
+    else {
+        let _request_user = get_request_user_data(session);
+        let _list_id : i32 = *_id;
+        let list = get_post_list(_list_id);
+        if list.user_id != _request_user.id {
+            Ok(HttpResponse::Ok()
+                .content_type("text/html; charset=utf-8")
+                .body(""))
         }
-    let body = YTemplate {
-        request_user: _request_user,
-        list: list,
+
+        #[derive(TemplateOnce)]
+        #[template(path = "desctop/posts/post_user/edit_list.stpl")]
+        struct YTemplate {
+            request_user: User,
+            list: PostList,
+            }
+        let body = YTemplate {
+            request_user: _request_user,
+            list: list,
+        }
+        .render_once()
+        .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+        Ok(HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(body))
     }
-    .render_once()
-    .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
-    Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(body))
 }
 pub async fn add_community_post_list_page(session: Session, req: HttpRequest, _id: web::Path<i32>) -> actix_web::Result<HttpResponse> {
     if is_signed_in(&session) {
@@ -109,8 +108,9 @@ pub async fn add_community_post_list_page(session: Session, req: HttpRequest, _i
         Ok(HttpResponse::Ok()
             .content_type("text/html; charset=utf-8")
             .body(body))
+    } else {
+        Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""))
     }
-    Ok(to_home())
 }
 pub async fn edit_community_post_list_page(session: Session, req: HttpRequest, _id: web::Path<i32>) -> actix_web::Result<HttpResponse> {
     if is_signed_in(&session) {
@@ -134,9 +134,11 @@ pub async fn edit_community_post_list_page(session: Session, req: HttpRequest, _
         .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
         Ok(HttpResponse::Ok()
             .content_type("text/html; charset=utf-8")
-            .body(body));
+            .body(body))
     }
-    Ok(to_home());
+    else {
+        Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""))
+    }
 }
 
 pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Result<HttpResponse> {
@@ -150,7 +152,7 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
 
     let params_some = web::Query::<GetListParams>::from_query(&req.query_string());
     if !params_some.is_ok() {
-        Ok(to_home());
+        Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""));
     }
     let params = params_some.unwrap();
     let list: PostList;
