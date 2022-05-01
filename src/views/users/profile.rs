7754c4b1985f0c03ccadbba4d0a6233e7910bb2b
interesnij <ochkarik1983@mail.ru>
2022-05-01 20:33:42 +0300

    J

--- src/views/users/profile.rs
+++ src/views/users/profile.rs
@@ -23,8 +23,8 @@ pub fn user_routes(config: &mut web::ServiceConfig) {
     config.route("/id{id}/", web::get().to(user_page));
 }
 
-pub fn my_user_account(type: String, user: User, request_user: User) -> actix_web::Result<HttpResponse> {
-    if _type == "desctop/".to_string() {
+pub fn my_user_account(folder: String, user: User, request_user: User) -> actix_web::Result<HttpResponse> {
+    if folder == "desctop/".to_string() {
         #[derive(TemplateOnce)]
         #[template(path = "desctop/users/account/my_user.stpl")]
         struct UserPage {
@@ -32,14 +32,12 @@ pub fn my_user_account(type: String, user: User, request_user: User) -> actix_we
             private_bools: Vec<bool>,
             request_user: User,
             user:         User,
-            is_my_user: bool,
         }
         let body = UserPage {
             title:        _user.get_full_name().clone(),
             private_bools: _user.get_profile_all_can_see(*_request_user_id).clone(),
             request_user: _request_user,
             user:         _user,
-            is_my_user:   is_my_user,
         }
         .render_once()
         .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
@@ -52,16 +50,12 @@ pub fn my_user_account(type: String, user: User, request_user: User) -> actix_we
             private_bools: Vec<bool>,
             request_user: User,
             user:         User,
-            user_profile: UserProfile,
-            is_my_user: bool,
         }
         let body = UserPage {
             title:        _user.get_full_name().clone(),
             private_bools: _user.get_profile_all_can_see(*_request_user_id).clone(),
             request_user: _request_user,
             user:         _user,
-            user_profile: _profile,
-            is_my_user:   is_my_user,
         }
         .render_once()
         .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
@@ -75,7 +69,6 @@ pub async fn user_page(session: Session, req: HttpRequest, _id: web::Path<i32>) 
     let _connection = establish_connection();
     let _type = get_folder(req);
     let _user = get_user(*_id);
-    let _profile = _user.get_profile();
 
     if is_signed_in(&session) {
         let _request_user = get_request_user_data(session);
