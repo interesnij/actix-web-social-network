5d74da76cd57bb54da1a4784d5dc124f0182e5d5
interesnij <ochkarik1983@mail.ru>
2022-05-02 14:33:42 +0300

    J

--- src/views/posts/post_pages.rs
+++ src/views/posts/post_pages.rs
@@ -180,8 +180,11 @@ pub async fn post_list_page(session: Session, req: HttpRequest) -> actix_web::Re
 
     if params.page.is_some() {
         let page = params.page.unwrap();
-        object_list = list.get_paginate_items(20, ((page - 1) * 20).into());
-        next_page_number = page + 1;
+        let step = (page - 1) * 20;
+        object_list = list.get_paginate_items(20, step).into());
+        if object_list.len() > step {
+            next_page_number = page + 1;
+        }
     }
     else {
         object_list = list.get_paginate_items(20, 0);
