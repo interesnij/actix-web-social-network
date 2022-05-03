ebe0e67c43e4af44551e9592fcbbbed7e0d74bc5
interesnij <ochkarik1983@mail.ru>
2022-05-03 11:10:37 +0300

    H

--- src/utils/progs.rs
+++ src/utils/progs.rs
@@ -268,28 +268,6 @@ static BAD_TOP: &str = "<div class='card mt-3 centered'><div class='card-body' s
 static BAD_MEDIUM: &str = "</svg></div><h6 style='text-align: center;margin-bottom: 20px;' class='text-center'>";
 static BAD_BOTTOM: &str = "</h6></div>";
 
-pub fn get_post_user_perm(user: User, request_user: User) -> (bool, String) {
-    if user.is_user_in_block(request_user.id) {
-        return (
-            false,
-            concat_string!(
-                BAD_TOP,
-                "<path d='M0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0z' fill='none' /><path d='M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z'/>",
-                BAD_MEDIUM,
-                "<a href='",
-                user.get_link(),
-                "' class='ajax underline'>",
-                user.get_full_name(),
-                "</a> ограничил",
-                user.get_gender_a(),
-                " доступ к своей странице.",
-                BAD_BOTTOM
-            )
-        )
-    }
-    else { return (true, "".to_string());}
-}
-
 pub fn get_user_permission(user: User, request_user: User)
     -> (bool, String) {
 
@@ -499,6 +477,179 @@ pub fn get_anon_user_permission(user: User) -> (bool, String) {
                 BAD_BOTTOM
             )
         )
+    }
+    else {
+        return (true, "".to_string())
+    }
+}
+
+
+pub fn get_community_permission(community: Community, request_user: User)
+    -> (bool, String) {
+
+    if request_user.types > 10 {
+        let chat_pk = request_user.get_or_create_support_chat_pk().to_string();
+        if request_user.is_closed() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/></g><g><path d='M14,8c0-2.21-1.79-4-4-4S6,5.79,6,8s1.79,4,4,4S14,10.21,14,8z M17,10v2h6v-2H17z M2,18v2h16v-2c0-2.66-5.33-4-8-4 S2,15.34,2,18z'/></g>",
+                    BAD_MEDIUM,
+                    "Ваша страница заблокирована за нарушение правил сайта. Если Вы не согласны с примененными к Вашей странице санкциями, напишите в <a href='/chat/",
+                    chat_pk,
+                    "/' class='ajax underline'> техподдержку </a>",
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else if request_user.is_deleted() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/></g><g><path d='M14,8c0-2.21-1.79-4-4-4S6,5.79,6,8s1.79,4,4,4S14,10.21,14,8z M17,10v2h6v-2H17z M2,18v2h16v-2c0-2.66-5.33-4-8-4 S2,15.34,2,18z'/></g>",
+                    BAD_MEDIUM,
+                    "Страница удалена. <br>Вы можете её <a class='pointer underline user_profile_restore'>восстановить</a>.",
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else if request_user.is_suspended() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/><path d='M17,12c-2.76,0-5,2.24-5,5s2.24,5,5,5c2.76,0,5-2.24,5-5S19.76,12,17,12z M18.65,19.35l-2.15-2.15V14h1v2.79l1.85,1.85 L18.65,19.35z M18,3h-3.18C14.4,1.84,13.3,1,12,1S9.6,1.84,9.18,3H6C4.9,3,4,3.9,4,5v15c0,1.1,0.9,2,2,2h6.11 c-0.59-0.57-1.07-1.25-1.42-2H6V5h2v3h8V5h2v5.08c0.71,0.1,1.38,0.31,2,0.6V5C20,3.9,19.1,3,18,3z M12,5c-0.55,0-1-0.45-1-1 c0-0.55,0.45-1,1-1c0.55,0,1,0.45,1,1C13,4.55,12.55,5,12,5z'/></g>",
+                    BAD_MEDIUM,
+                    "Ваша страница будет разморожена",
+                    request_user.get_longest_penalties(),
+                    "Если Вы не согласны с примененными к Вашей странице санкциями, напишите в <a href='/chat/",
+                    chat_pk,
+                    "/' class='ajax underline'> техподдержку </a>",
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else { return (true, "".to_string());}
+    }
+
+    else if community.types > 10 {
+        if community.is_closed() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/></g><g><path d='M14,8c0-2.21-1.79-4-4-4S6,5.79,6,8s1.79,4,4,4S14,10.21,14,8z M17,10v2h6v-2H17z M2,18v2h16v-2c0-2.66-5.33-4-8-4 S2,15.34,2,18z'/></g>",
+                    BAD_MEDIUM,
+                    "<a href='",
+                    community.get_link(),
+                    "'>",
+                    community.name,
+                    "</a> заблокировано за нарушение правил сайта.",
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else if community.is_deleted() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/></g><g><path d='M14,8c0-2.21-1.79-4-4-4S6,5.79,6,8s1.79,4,4,4S14,10.21,14,8z M17,10v2h6v-2H17z M2,18v2h16v-2c0-2.66-5.33-4-8-4 S2,15.34,2,18z'/></g>",
+                    BAD_MEDIUM,
+                    "Сщщбщество <a href='",
+                    community.get_link(),
+                    "'>",
+                    community.name,
+                    "</a> удалено.",
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else if community.is_suspended() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/><path d='M17,12c-2.76,0-5,2.24-5,5s2.24,5,5,5c2.76,0,5-2.24,5-5S19.76,12,17,12z M18.65,19.35l-2.15-2.15V14h1v2.79l1.85,1.85 L18.65,19.35z M18,3h-3.18C14.4,1.84,13.3,1,12,1S9.6,1.84,9.18,3H6C4.9,3,4,3.9,4,5v15c0,1.1,0.9,2,2,2h6.11 c-0.59-0.57-1.07-1.25-1.42-2H6V5h2v3h8V5h2v5.08c0.71,0.1,1.38,0.31,2,0.6V5C20,3.9,19.1,3,18,3z M12,5c-0.55,0-1-0.45-1-1 c0-0.55,0.45-1,1-1c0.55,0,1,0.45,1,1C13,4.55,12.55,5,12,5z'/></g>",
+                    BAD_MEDIUM,
+                    "Сообщество будет разморожено ",
+                    community.get_longest_penalties(),
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else { return (true, "".to_string());}
+    }
+    else if request_user.is_banned_from_community(community.id) {
+        return (
+            false,
+            concat_string!(
+                BAD_TOP,
+                "<path d='M0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0z' fill='none' /><path d='M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z'/>",
+                BAD_MEDIUM,
+                "<a href='",
+                community.get_link(),
+                "' class='ajax underline'>",
+                community.name,
+                "</a> добавило Вас в чёрный список",
+                BAD_BOTTOM
+            )
+        )
+    }
+    else {
+        return (true, "".to_string())
+    }
+}
+
+pub fn get_anon_community_permission(community: Community) -> (bool, String) {
+    if community.types > 10 {
+        if community.is_closed() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/></g><g><path d='M14,8c0-2.21-1.79-4-4-4S6,5.79,6,8s1.79,4,4,4S14,10.21,14,8z M17,10v2h6v-2H17z M2,18v2h16v-2c0-2.66-5.33-4-8-4 S2,15.34,2,18z'/></g>",
+                    BAD_MEDIUM,
+                    "<a href='",
+                    community.get_link(),
+                    "'>",
+                    community.name,
+                    "</a> заблокировано за нарушение правил сайта.",
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else if community.is_deleted() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/></g><g><path d='M14,8c0-2.21-1.79-4-4-4S6,5.79,6,8s1.79,4,4,4S14,10.21,14,8z M17,10v2h6v-2H17z M2,18v2h16v-2c0-2.66-5.33-4-8-4 S2,15.34,2,18z'/></g>",
+                    BAD_MEDIUM,
+                    "Сщщбщество <a href='",
+                    community.get_link(),
+                    "'>",
+                    community.name,
+                    "</a> удалено.",
+                    BAD_BOTTOM
+                )
+            )
+        }
+        else if community.is_suspended() {
+            return (
+                false,
+                concat_string!(
+                    BAD_TOP,
+                    "<g><rect fill='none' height='24' width='24'/><path d='M17,12c-2.76,0-5,2.24-5,5s2.24,5,5,5c2.76,0,5-2.24,5-5S19.76,12,17,12z M18.65,19.35l-2.15-2.15V14h1v2.79l1.85,1.85 L18.65,19.35z M18,3h-3.18C14.4,1.84,13.3,1,12,1S9.6,1.84,9.18,3H6C4.9,3,4,3.9,4,5v15c0,1.1,0.9,2,2,2h6.11 c-0.59-0.57-1.07-1.25-1.42-2H6V5h2v3h8V5h2v5.08c0.71,0.1,1.38,0.31,2,0.6V5C20,3.9,19.1,3,18,3z M12,5c-0.55,0-1-0.45-1-1 c0-0.55,0.45-1,1-1c0.55,0,1,0.45,1,1C13,4.55,12.55,5,12,5z'/></g>",
+                    BAD_MEDIUM,
+                    "Сообщество будет разморожено ",
+                    community.get_longest_penalties(),
+                    BAD_BOTTOM
+                )
+            )
+        }
     }
     else {
         return (true, "".to_string())
