772bf310bd32c51526ae0cfe1ccd58d2b7145e61
interesnij <ochkarik1983@mail.ru>
2022-05-03 13:22:50 +0300

    H

--- static/scripts/functions/reload.js
+++ static/scripts/functions/reload.js
@@ -652,12 +652,7 @@ function if_list(block) {
   // прога подгружает списки чего либо при подгрузке страницы, а также подтягивает окна
     if (block.querySelector('.load_post_list')) {
         _block = block.querySelector('.load_post_list');
-        if (_block.getAttribute("user-pk")) {
-          link = "/posts/list/?user=" + _block.getAttribute("user-pk")
-        } else if (_block.getAttribute("community-pk")){
-          link = "/posts/list/?community=" + _block.getAttribute("community-pk")
-        };
-        list_block_load(_block, ".load_list", link);
+        list_block_load(_block, ".load_list", _block.getAttribute("data-link"));
         scrolled(_block.querySelector('.list_pk'));
     } else if (block.querySelector('.is_block_post_paginate')) {
         lenta = block.querySelector('.is_block_post_paginate');
