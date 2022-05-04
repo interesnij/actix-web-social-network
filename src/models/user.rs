c0f2011710a670c0fc19807760deb5918707b99c
interesnij <ochkarik1983@mail.ru>
2022-05-04 20:01:09 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -2728,7 +2728,7 @@ impl User {
             return "Предупреждение за нарушение правил соцсети трезвый.рус".to_string();
         }
     }
-    pub fn get_all_chats(&self, limit: i64, offset: i64) -> Vec<Chat> {
+    pub fn get_all_chats(&self, limit: i64, offset: i64) -> Vec<&Chat> {
         use crate::schema::chat_users::dsl::chat_users;
         use crate::schema::chats::dsl::chats;
         use crate::models::ChatUser;
@@ -2755,7 +2755,7 @@ impl User {
         let mut chats: Vec<Chat>;
         for chat in chat_list.iter() {
             if chat.is_group() || chat.is_public() || chat.is_not_empty() {
-                chats.push(&chat);
+                chats.push(chat);
             }
         }
         return chats;
