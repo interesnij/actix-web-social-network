ff5607e2adbd311fed1cd697e8d825eb29965680
interesnij <ochkarik1983@mail.ru>
2022-05-04 20:46:07 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -2754,7 +2754,9 @@ impl User {
 
         for chat in chat_list.iter() {
             if !chat.is_group() || !chat.is_public() || !chat.is_not_empty() {
-                chat_list.retain(|&x.id| x != chat.id);
+                let index = chat_list.iter().position(|x| *x == chat).unwrap();
+                chat_list.remove(index);
+                //chat_list.retain(|&x| x != chat);
             }
         }
         return chat_list;
