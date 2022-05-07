84ac28f44fc798a4b0522e386638df210ea118a8
interesnij <ochkarik1983@mail.ru>
2022-05-07 15:54:33 +0300

    J

--- src/routes.rs
+++ src/routes.rs
@@ -13,7 +13,7 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
     .configure(chats::chats_routes)
 
     .configure(pages::pages_routes)
-    .configure(profile::user_routes)
+    .configure(user_routes)
 
     .configure(post_pages::post_routes)
     .configure(post_progs::post_progs)
