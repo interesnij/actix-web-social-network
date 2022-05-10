aee09a9fa2bb7f9d8d45d5732c1c5639360cb552
interesnij <ochkarik1983@mail.ru>
2022-05-10 19:21:10 +0300

    J

--- src/views/photos/pages.rs
+++ src/views/photos/pages.rs
@@ -13,7 +13,7 @@ use crate::utils::{
     get_request_user_data,
     get_user,
     get_community,
-    get_doc_list,
+    get_photo_list,
     get_user_permission,
     get_anon_user_permission,
     get_community_permission,
