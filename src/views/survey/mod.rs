02ef8ae30712fa8b84c485aead96ec750d0df9eb
interesnij <ochkarik1983@mail.ru>
2022-05-11 08:28:38 +0300

    J

--- src/views/survey/mod.rs
+++ src/views/survey/mod.rs
@@ -1,13 +1,13 @@
-pub mod pages;
+//pub mod pages;
 
-use actix_web::web::ServiceConfig;
+//use actix_web::web::ServiceConfig;
 
-pub use self::{
-    pages::*,
-};
+//pub use self::{
+//    pages::*,
+//};
 
-pub fn survey_routes(cfg: &mut ServiceConfig) {
-    cfg
-    .configure(surveys_urls)
-    ;
-}
+//pub fn survey_routes(cfg: &mut ServiceConfig) {
+//    cfg
+//    .configure(surveys_urls)
+//    ;
+//}
