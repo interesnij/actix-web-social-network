eb4c6faf27e06eb2d794b7ddbbff7f9ed1538264
interesnij <ochkarik1983@mail.ru>
2022-05-11 15:25:53 +0300

    J

--- src/views/posts/post_pages.rs
+++ src/views/posts/post_pages.rs
@@ -155,10 +155,9 @@ pub async fn load_post_page(session: Session, req: HttpRequest, post_id: web::Pa
     let _list = get_post_list(_post.post_list_id);
 
     let _posts = _list.get_items();
-    let _posts_len = _list.count;
     for (i, item) in _posts.iter().enumerate().rev() {
         if item.id == _post.id {
-            if (i + 1) != _posts_len {
+            if (i + 1) != _list.count {
                 _prev = Some(&_posts[i + 1].id);
             };
             if i != 0 {
