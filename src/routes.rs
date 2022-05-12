84e9756bbf64b3eaa59438fc5b7d4ed24e17d7dd
interesnij <ochkarik1983@mail.ru>
2022-05-12 21:48:02 +0300

    H

--- src/routes.rs
+++ src/routes.rs
@@ -3,7 +3,7 @@ use actix_web::web;
 use crate::views::{
     pages,
     auth,
-    user_routes,
+    //user_routes,
     //community_routes,
     post_routes,
     //chat_routes,
@@ -19,7 +19,7 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg
     .configure(pages::pages_routes)
     .configure(auth::auth_routes)
-    .configure(user_routes)
+    //.configure(user_routes)
     //.configure(community_routes)
     .configure(post_routes)
     //.configure(chat_routes)
