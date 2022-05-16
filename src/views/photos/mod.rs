02b8ab33b2b33a66db6ffa0f7d0d2e6d1e2688cd
interesnij <ochkarik1983@mail.ru>
2022-05-16 15:35:22 +0300

    J

--- src/views/photos/mod.rs
+++ src/views/photos/mod.rs
@@ -1,16 +1,16 @@
 pub mod pages;
-//pub mod photo_progs;
+pub mod photo_progs;
 
 use actix_web::web::ServiceConfig;
 
 pub use self::{
     pages::*,
-    //photo_progs::*,
+    photo_progs::*,
 };
 
 pub fn photos_routes(cfg: &mut ServiceConfig) {
     cfg
     .configure(photos_urls)
-    //.configure(photo_progs_urls)
+    .configure(photo_progs_urls)
     ;
 }
