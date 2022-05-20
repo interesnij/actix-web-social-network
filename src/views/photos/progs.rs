3aad2377b4fbfd39529ec9c5db0d4ea4e97ec7a9
interesnij <ochkarik1983@mail.ru>
2022-05-20 21:22:52 +0300

    m

--- src/views/photos/progs.rs
+++ src/views/photos/progs.rs
@@ -25,7 +25,7 @@ use std;
 use std::{borrow::BorrowMut, io::Write};
 use actix_multipart::{Field, Multipart};
 use futures::StreamExt;
-use crate::diesel::RunQueryDsl;
+use crate::diesel::{RunQueryDsl,ExpressionMethods};
 
 
 pub fn progs_urls(config: &mut web::ServiceConfig) {
