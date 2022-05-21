d7e01716cbb18f2d806a5b29e835d21a587bb64c
interesnij <ochkarik1983@mail.ru>
2022-05-21 15:59:59 +0300

    J

--- src/views/goods/pages.rs
+++ src/views/goods/pages.rs
@@ -662,8 +662,8 @@ pub async fn add_good_in_list_page(session: Session, req: HttpRequest, _id: web:
         let _connection = establish_connection();
 
         let categories = good_subcategories
-            .load::<GoodSubcategorie>(&_connection)
             .order(schema::good_subcategories::position.desc())
+            .load::<GoodSubcategorie>(&_connection)
             .expect("E.");
 
         let _request_user = get_request_user_data(session);
