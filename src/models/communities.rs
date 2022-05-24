cb0955ed32ded260bbe9647da88605da68a18ccf
interesnij <ochkarik1983@mail.ru>
2022-05-24 12:47:46 +0300

    J

--- src/models/communities.rs
+++ src/models/communities.rs
@@ -275,49 +275,49 @@ impl Community {
     pub fn is_community_list(&self, types: &String) -> bool {
         let pk: i32 = types[3..].parse().unwrap();
         let code = &types[..3];
-        if types == "lpo".to_string() {
+        if types == &"lpo".to_string() {
             use crate::utils::get_post_list;
             let list = get_post_list(pk);
             if list.community_id.is_some() && list.community_id.unwrap() == self.id {
                 return true;
             }
         }
-        else if types == "lgo".to_string() {
+        else if types == &"lgo".to_string() {
             use crate::utils::get_good_list;
             let list = get_good_list(pk);
             if list.community_id.is_some() && list.community_id.unwrap() == self.id {
                 return true;
             }
         }
-        else if types == "lph".to_string() {
+        else if types == &"lph".to_string() {
             use crate::utils::get_photo_list;
             let list = get_photo_list(pk);
             if list.community_id.is_some() && list.community_id.unwrap() == self.id {
                 return true;
             }
         }
-        else if types == "lvi".to_string() {
+        else if types == &"lvi".to_string() {
             use crate::utils::get_video_list;
             let list = get_video_list(pk);
             if list.community_id.is_some() && list.community_id.unwrap() == self.id {
                 return true;
             }
         }
-        else if types == "lsu".to_string() {
+        else if types == &"lsu".to_string() {
             use crate::utils::get_survey_list;
             let list = get_survey_list(pk);
             if list.community_id.is_some() && list.community_id.unwrap() == self.id {
                 return true;
             }
         }
-        else if types == "ldo".to_string() {
+        else if types == &"ldo".to_string() {
             use crate::utils::get_doc_list;
             let list = get_doc_list(pk);
             if list.community_id.is_some() && list.community_id.unwrap() == self.id {
                 return true;
             }
         }
-        else if types == "lmu".to_string() {
+        else if types == &"lmu".to_string() {
             use crate::utils::get_music_list;
             let list = get_music_list(pk);
             if list.community_id.is_some() && list.community_id.unwrap() == self.id {
@@ -329,37 +329,37 @@ impl Community {
     pub fn is_list_in_collection(&self, types: &String) -> bool {
         let pk: i32 = types[3..].parse().unwrap();
         let code = &types[..3];
-        if types == "lpo".to_string() {
+        if types == &"lpo".to_string() {
             use crate::utils::get_post_list;
             let list = get_post_list(pk);
             return list.get_communities_ids().iter().any(|&i| i==self.id);
         }
-        else if types == "lgo".to_string() {
+        else if types == &"lgo".to_string() {
             use crate::utils::get_good_list;
             let list = get_good_list(pk);
             return list.get_communities_ids().iter().any(|&i| i==self.id);
         }
-        else if types == "lph".to_string() {
+        else if types == &"lph".to_string() {
             use crate::utils::get_photo_list;
             let list = get_photo_list(pk);
             return list.get_communities_ids().iter().any(|&i| i==self.id);
         }
-        else if types == "lvi".to_string() {
+        else if types == &"lvi".to_string() {
             use crate::utils::get_video_list;
             let list = get_video_list(pk);
             return list.get_communities_ids().iter().any(|&i| i==self.id);
         }
-        else if types == "lsu".to_string() {
+        else if types == &"lsu".to_string() {
             use crate::utils::get_survey_list;
             let list = get_survey_list(pk);
             return list.get_communities_ids().iter().any(|&i| i==self.id);
         }
-        else if types == "ldo".to_string() {
+        else if types == &"ldo".to_string() {
             use crate::utils::get_doc_list;
             let list = get_doc_list(pk);
             return list.get_communities_ids().iter().any(|&i| i==self.id);
         }
-        else if types == "lmu".to_string() {
+        else if types == &"lmu".to_string() {
             use crate::utils::get_music_list;
             let list = get_music_list(pk);
             return list.get_communities_ids().iter().any(|&i| i==self.id);
