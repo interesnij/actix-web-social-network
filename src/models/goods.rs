58ff69ec59a320a27e52d536fcbdc2ca57a2fc38
interesnij <ochkarik1983@mail.ru>
2022-05-29 13:21:30 +0300

    J

--- src/models/goods.rs
+++ src/models/goods.rs
@@ -3593,7 +3593,7 @@ impl GoodReaction {
         new_types: i16,
         old_types_option: Option<i16>,
         plus: bool,
-    ) -> bool {
+    ) -> GoodReaction {
         let _connection = establish_connection();
         if old_types_option.is_some() {
             let old_types = old_types_option.unwrap();
@@ -3662,7 +3662,7 @@ impl GoodReaction {
                     .set(schema::good_reactions::pile_of_poo.eq(self.pile_of_poo + 1))
                     .get_result::<GoodReaction>(&_connection)
                     .expect("Error."),
-                _ => false,
+                _ => self,
             };
 
             let update_model = match old_types {
@@ -3730,7 +3730,7 @@ impl GoodReaction {
                     .set(schema::good_reactions::pile_of_poo.eq(self.pile_of_poo - 1))
                     .get_result::<GoodReaction>(&_connection)
                     .expect("Error."),
-                _ => false,
+                _ => self,
             };
         }
         else {
@@ -3800,7 +3800,7 @@ impl GoodReaction {
                         .set(schema::good_reactions::pile_of_poo.eq(self.pile_of_poo + 1))
                         .get_result::<GoodReaction>(&_connection)
                         .expect("Error."),
-                    _ => false,
+                    _ => self,
                 };
             }
             else {
@@ -3869,10 +3869,10 @@ impl GoodReaction {
                         .set(schema::good_reactions::pile_of_poo.eq(self.pile_of_poo - 1))
                         .get_result::<GoodReaction>(&_connection)
                         .expect("Error."),
-                    _ => false,
+                    _ => self,
                 };
             }
-            return true;
+            return self;
         }
     }
 }
