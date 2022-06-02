03929490fe6404dae1554fb5ab87ab86f283c3f7
interesnij <ochkarik1983@mail.ru>
2022-06-02 14:36:24 +0300

    J

--- src/models/posts.rs
+++ src/models/posts.rs
@@ -2401,7 +2401,7 @@ impl Post {
             else {
                 let first_str = &unwrap[..100].to_string();
                 let last_str = &unwrap[101..].to_string();
-                let new_str = first_str + &"<br><a class='pointer show_post_text'>Показать полностью...</a><br><span style='display:none'>" + &last_str + &"</span>";
+                let new_str = first_str.to_owned() + &"<br><a class='pointer show_post_text'>Показать полностью...</a><br><span style='display:none'>" + &last_str + &"</span>";
                 return new_str;
             }
         } else { return "".to_string(); }
