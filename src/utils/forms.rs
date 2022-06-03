104419c5a9efdea0cee2d26b8e8ef685ee7c54d8
interesnij <ochkarik1983@mail.ru>
2022-06-03 11:56:14 +0300

    J

--- src/utils/forms.rs
+++ src/utils/forms.rs
@@ -143,30 +143,36 @@ pub async fn post_list_form(
             while let Some(chunk) = field.next().await {
                 let data = chunk.expect("split_payload err chunk");
                 if let Ok(s) = str::from_utf8(&data) {
-                    let data_string = s.to_string();
                     if field.name() == "name" {
+                        let data_string = s.to_string();
                         form.name = data_string
                     } else if field.name() == "description" {
-                        if data_string != "".to_string() {
-                            form.description = Some(data_string);
+                        if s.is_some() {
+                            form.description = Some(s);
                         }
                     }
                     else if field.name() == "can_see_el" {
+                        let data_string = s.to_string();
                         form.can_see_el = data_string
                     }
                     else if field.name() == "can_see_comment" {
+                        let data_string = s.to_string();
                         form.can_see_comment = data_string
                     }
                     else if field.name() == "create_el" {
+                        let data_string = s.to_string();
                         form.create_el = data_string
                     }
                     else if field.name() == "create_comment" {
+                        let data_string = s.to_string();
                         form.create_comment = data_string
                     }
                     else if field.name() == "copy_el" {
+                        let data_string = s.to_string();
                         form.copy_el = data_string
                     }
                     else if field.name() == "reactions" {
+                        let data_string = s.to_string();
                         form.reactions = Some(data_string);
                     }
                 }
