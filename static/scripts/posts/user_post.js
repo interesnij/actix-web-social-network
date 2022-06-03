62a3503e1bdb2c815d97b089a2350602435110b0
interesnij <ochkarik1983@mail.ru>
2022-06-03 13:33:22 +0300

    J

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -848,10 +848,7 @@ on('#ajax', 'click', '#edit_list_btn', function() {
   react_value = form.querySelector(".reactions_collector");
 
   _value = react_value.value;
-  if (!_value) {
-    react_value.remove();
-  }
-  else if (_value.slice(-1) == " ") {
+  if (_value.slice(-1) == " ") {
     console.log(true);
     react_value.value = _value.slice(0, -2);
   }
