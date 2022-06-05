ebd1aaf7e11f383d9a7c90e15ea24ac20a17bb59
interesnij <ochkarik1983@mail.ru>
2022-06-05 17:05:17 +0300

    J

--- src/views/chats/c_pages.rs
+++ src/views/chats/c_pages.rs
@@ -28,7 +28,9 @@ pub fn c_pages_urls(config: &mut web::ServiceConfig) {
     config.route("/chat/load_chat_message/{id}/", web::get().to(load_chat_message_page));
     config.route("/chat/load_message/{id}/", web::get().to(load_message_page));
     config.route("/chat/edit_message/{id}/", web::get().to(edit_message_page));
-
+    config.route("/chat/edit_message/{id}/", web::get().to(edit_message_page));
+    config.route("/chat/exclude_users/{id}/", web::get().to(chat_exclude_users_load));
+    config.route("/chat/include_users/{id}/", web::get().to(chat_include_users_load));
 }
 
 pub async fn chats_list_page(session: Session, req: HttpRequest) -> actix_web::Result<HttpResponse> {
@@ -511,6 +513,143 @@ pub async fn chat_exclude_users_load(session: Session, req: HttpRequest, _id: we
 
             #[derive(TemplateOnce)]
             #[template(path = "mobile/chats/chat/info/exclude_users.stpl")]
+            struct Template {
+                request_user:     User,
+                chat:             Chat,
+                object_list:      Vec<User>,
+                users:            Vec<User>,
+                next_page_number: i32,
+                types:            String,
+                count:            i32,
+                text:             String,
+            }
+
+            let body = Template {
+                request_user:     _request_user,
+                chat:             _chat,
+                object_list:      object_list,
+                users:            users_list,
+                next_page_number: next_page_number,
+                types:            types,
+                count:            count,
+                text:             text,
+            }
+            .render_once()
+            .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+            Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+        }
+    } else {
+        Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(""))
+    }
+}
+
+pub async fn chat_include_users_load(session: Session, req: HttpRequest, _id: web::Path<i32>) -> actix_web::Result<HttpResponse> {
+    #[derive(Debug, Deserialize)]
+    pub struct ZParams {
+        pub page:  Option<i32>,
+        pub types: Option<String>,
+    }
+
+    let params_some = web::Query::<ZParams>::from_query(&req.query_string());
+    let (is_desctop, page) = get_list_variables(req);
+
+    if is_signed_in(&session) {
+        let mut page: i32 = 1;
+        let mut next_page_number: i32 = 0;
+        let mut text =  "".to_string();
+        let mut types = "".to_string();
+        let _chat = get_chat(*_id);
+
+        if params_some.is_ok() {
+            let params = params_some.unwrap();
+            if params.page.is_some() {
+                page = params.page.unwrap();
+            }
+            if params.types.is_some() {
+                types = params.types.as_ref().unwrap().to_string();
+            }
+        }
+
+        let _request_user = get_request_user_data(session);
+        let mut users_list: Vec<User> = Vec::new();
+        let mut object_list: Vec<User> = Vec::new();
+
+        let count = _chat.members;
+            if page > 1 {
+                let step = (page - 1) * 20;
+                object_list = _chat.get_members(20, step.into());
+                if count > (page * 20).try_into().unwrap() {
+                    next_page_number = page + 1;
+                }
+            }
+            else {
+                object_list = _chat.get_members(20, 0);
+                if count > 20.try_into().unwrap() {
+                    next_page_number = 2;
+                }
+        }
+
+        if types == "can_add_members".to_string() {
+            text = "добавлять участников".to_string();
+            users_list = _chat.get_can_add_in_chat_include_users()
+        }
+        else if types == "can_fix_item".to_string() {
+            text = "закреплять сообщения".to_string();
+            users_list = _chat.get_can_add_fix_include_users()
+        }
+        else if types == "can_mention".to_string() {
+            text = "упоминать о чате".to_string();
+            users_list = _chat.get_can_send_mention_include_users()
+        }
+        else if types == "can_add_admin".to_string() {
+            text = "добавлять админов".to_string();
+            users_list = _chat.get_can_add_admin_include_users()
+        }
+        else if types == "can_add_design".to_string() {
+            text = "работать с дизайном".to_string();
+            users_list = _chat.get_can_add_design_include_users()
+        }
+        else if types == "can_see_settings".to_string() {
+            text = "видеть настройки".to_string();
+            users_list = _chat.get_can_see_settings_include_users()
+        }
+        else if types == "can_see_log".to_string() {
+            text = "видеть логи чата".to_string();
+            users_list = _chat.get_can_see_log_include_users()
+        }
+
+        if is_desctop {
+            #[derive(TemplateOnce)]
+            #[template(path = "desctop/chats/chat/info/include_users.stpl")]
+            struct Template {
+                request_user:     User,
+                chat:             Chat,
+                object_list:      Vec<User>,
+                users:            Vec<User>,
+                next_page_number: i32,
+                types:            String,
+                count:            i32,
+                text:             String,
+            }
+
+            let body = Template {
+                request_user:     _request_user,
+                chat:             _chat,
+                object_list:      object_list,
+                users:            users_list,
+                next_page_number: next_page_number,
+                types:            types,
+                count:            count,
+                text:             text,
+            }
+            .render_once()
+            .map_err(|e| InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR))?;
+            Ok(HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body))
+
+        } else {
+
+            #[derive(TemplateOnce)]
+            #[template(path = "mobile/chats/chat/info/include_users.stpl")]
             struct Template {
                 request_user:     User,
                 chat:             Chat,
