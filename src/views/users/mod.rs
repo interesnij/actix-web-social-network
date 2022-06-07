b06edd34278b91bfdf801c694f5e7a318cbd2b17
interesnij <ochkarik1983@mail.ru>
2022-06-07 19:30:05 +0300

    J

--- src/views/users/mod.rs
+++ src/views/users/mod.rs
@@ -1,27 +1,28 @@
 pub mod profile;
 pub mod templ;
+pub mod progs;
 pub mod settings;
 pub mod pages;
 pub mod load;
-pub mod progs;
 
 use actix_web::web::ServiceConfig;
 
 pub use self::{
     profile::*,
     templ::*,
+    progs::*,
     settings::*,
     pages::*,
     load::*,
-    progs::*,
+
 };
 
 pub fn user_routes(cfg: &mut ServiceConfig) {
     cfg
     .configure(profile_urls)
     .configure(pages_urls)
+    .configure(progs_urls)
     .configure(load_urls)
     .configure(settings_urls)
-    .configure(progs_urls)
     ;
 }
