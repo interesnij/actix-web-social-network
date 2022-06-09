6375183e4e04abb04ee3e10703aa3b0b710ab4b0
interesnij <ochkarik1983@mail.ru>
2022-06-09 13:37:55 +0300

    J

--- src/utils/attach.rs
+++ src/utils/attach.rs
@@ -30,10 +30,12 @@ pub fn add_post(pk: i32, user_id: i32, is_staff: bool) -> String {
             let mut border_radius = "".to_string();
             if count != 0 {
                 let count_str = count.to_string();
+
                 let users = post.get_6_reactions_users_of_types(*reaction);
                 if &user_reaction == reaction {
                     border_radius = "border_radius".to_string();
                 }
+                let reaction_str = reaction.to_string();
                 let mut users_html = "".to_string();
                 for user in users.iter() {
                     users_html = concat_string!(
@@ -45,10 +47,10 @@ pub fn add_post(pk: i32, user_id: i32, is_staff: bool) -> String {
                     );
                 }
                 reacts = concat_string!(
-                    reacts, "<span class='react' data-react='", reaction,
+                    reacts, "<span class='react' data-react='", reaction_str,
                     "'><span class='like send_react ", border_radius,
                     "<img style='width:17px' src='/static/images/reactions/",
-                    reaction, ".png' alt='img' /><span class='reactions_count'>",
+                    reaction_str, ".png' alt='img' /><span class='reactions_count'>",
                     count_str, "</span></span><span class='like_window'><div class='like_pop'>
                     <span class='item_reactions pointer'>Отреагировали: <span data-count='like'>",
                     count_str, "<span class='like_list' style='display:flex;flex-wrap:wrap;margin-top:10px;'>",
