ce6619b15ee5d3fec741f879b0ea3b431dbeae9f
interesnij <ochkarik1983@mail.ru>
2022-06-13 21:47:56 +0300

    J

--- static/scripts/anon.js
+++ static/scripts/anon.js
@@ -44,7 +44,7 @@ function create_fullscreen(url, type_class) {
   $parent_div.classList.add("card_fullscreen", "mb-3", "border", type_class);
   $parent_div.style.zIndex = 100 + count_items;
   $parent_div.style.opacity = "0";
-  document.querySelector("title") = document.querySelector(".main_container").getAttribute("data-title");
+  document.title = document.body.querySelector(".main_container").getAttribute("data-title"); 
 
   if (document.body.querySelector(".desctop_nav")) {
     hide_svg = '<svg class="svg_default" style="position:fixed;" width="30" height="30" fill="currentColor" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'
