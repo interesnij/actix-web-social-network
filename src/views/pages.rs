8e3ad026aeec780d87dcc8d59b5f600987f15e60
interesnij <ochkarik1983@mail.ru>
2022-06-14 11:54:29 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -1339,10 +1339,10 @@ pub async fn all_reactions_page(session: Session, req: HttpRequest) -> actix_web
     }
 }
 
-pub async fn mobile_menu_page(session: Session) -> actix_web::Result<HttpResponse> {
+pub async fn mobile_menu_page(session: Session, req: HttpRequest) -> actix_web::Result<HttpResponse> {
     if is_signed_in(&session) {
         use crate::utils::get_ajax;
-        
+
         let is_ajax = get_ajax(&req);
         #[derive(TemplateOnce)]
         #[template(path = "mobile/main/menu.stpl")]
