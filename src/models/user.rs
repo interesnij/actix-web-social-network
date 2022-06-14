b20047812fcde9c668720c63cf244570f2d0925a
interesnij <ochkarik1983@mail.ru>
2022-06-14 12:22:24 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -888,8 +888,8 @@ impl User {
             false => "&nbsp;<svg style='width: 17px;' class='svg_default' fill='currentColor' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z'/></svg>",
         };
         let gender = match self.is_men() {
-            true => "<i>Был ",
-            false => "<i>Была ",
+            true => "<i>Был </i>",
+            false => "<i>Была </i>",
         };
         if self.is_online() == true {
             return "<i>Онлайн</i>".to_string() + &device;
