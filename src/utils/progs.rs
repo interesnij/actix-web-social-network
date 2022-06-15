a28b837743f080a4b7457b6335da1429811b7609
interesnij <ochkarik1983@mail.ru>
2022-06-15 21:37:29 +0300

    J

--- src/utils/progs.rs
+++ src/utils/progs.rs
@@ -1017,12 +1017,13 @@ pub fn get_formatted_text(text: &str) -> String {
                         }
                     }
                 }
-                if exists {
+                if exists && !_exlude.iter().any(|&i| i==word) {
                     println!("упоминание есть!!");
                     _loop.push("".to_string());
                     next += 1;
                     _loop[next] = _loop[this].replace(&(indent.to_owned() + &word), &(indent.to_owned() + &"<a class='action ajax show_mention_info pointer'href='/".to_string() + &word[1..].to_string() + &"/'>".to_string() + &name + &"</a>".to_string()));
                     this += 1;
+                    _exlude.push(word.to_string());
                 }
             }
 
