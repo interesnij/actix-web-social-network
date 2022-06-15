e328a35bb8a23e1f08cf6b24001e337e472102c2
interesnij <ochkarik1983@mail.ru>
2022-06-15 16:04:22 +0300

    J

--- src/utils/progs.rs
+++ src/utils/progs.rs
@@ -927,7 +927,7 @@ pub fn get_formatted_text(text: &str) -> String {
                 next += 1;
 
                 //if word[0] == "#".to_string() {
-                _loop[next] = _loop[this].replace(indent.to_owned() + &word, indent.to_owned() + "<a class='ajax action'href='/search/?q=%23'" + word + "'>" + word + "</a>", 1).replace("#<", "<");
+                _loop[next] = &_loop[this].replace(indent.to_owned() + &word, indent.to_owned() + "<a class='ajax action'href='/search/?q=%23'" + word + "'>" + word + "</a>", 1).replace("#<", "<");
                 //}
             }
             else if word[0] == "@" {
@@ -1012,7 +1012,7 @@ pub fn get_formatted_text(text: &str) -> String {
                     _loop.append("".to_string());
                     this += 1;
                     next += 1;
-                    _loop[next] = _loop[this].replace(indent.to_owned() + &word, indent.to_owned() + &"<a class='action ajax show_mention_info pointer'href='/'".to_string() + &word[..0].to_string() + &"/'>".to_string() + &name + &"</a>".to_string(), 1);
+                    _loop[next] = &_loop[this].replace(indent.to_owned() + &word, indent.to_owned() + &"<a class='action ajax show_mention_info pointer'href='/'".to_string() + &word[..0].to_string() + &"/'>".to_string() + &name + &"</a>".to_string(), 1);
                 }
             }
 
@@ -1022,14 +1022,14 @@ pub fn get_formatted_text(text: &str) -> String {
                     p_2 = word;
                 }
                 else {
-                    p_2 = "//".to_owned() + &word;
+                    p_2 = &("//".to_owned() + &word);
                 }
                 if word.contains("трезвый.рус") {
                     _loop.push("");
-                    p_2 = word.replace("трезвый.рус", "/").replace("http://", "").replace("https://", "");
+                    p_2 = &word.replace("трезвый.рус", "/").replace("http://", "").replace("https://", "");
                     this += 1;
                     next += 1;
-                    _loop[next] = _loop[this].replace(indent.to_owned() + &word, indent.to_owned() + &"<a class='ajax action'href='" + &p_2 + &"'>".to_string() + &word[..30].to_string() + &"</a>".to_string(), 1);
+                    _loop[next] = &_loop[this].replace(indent.to_owned() + &word, indent.to_owned() + &"<a class='ajax action'href='" + &p_2 + &"'>".to_string() + &word[..30].to_string() + &"</a>".to_string(), 1);
                 }
                 else {
                     for zone in zons {
@@ -1039,14 +1039,14 @@ pub fn get_formatted_text(text: &str) -> String {
                                 _loop.push("");
                                 this += 1;
                                 next += 1;
-                                _loop[next] = _loop[this].replace(word, "<a class='action'rel='nofollow'target='_blank'href='".to_string() + &p_2 + &"'>".to_string() + &word[..30].to_string() + &"</a>".to_string(), 1);
+                                _loop[next] = &_loop[this].replace(word, "<a class='action'rel='nofollow'target='_blank'href='".to_string() + &p_2 + &"'>".to_string() + &word[..30].to_string() + &"</a>".to_string(), 1);
                             }
                         }
                     }
                 }
             }
         }
-        let result = _loop[next].replace(" |<imgsrc","<img src").replace(".png\">| ",".png\">").replace(" <br> ","<br>");
+        let result = &_loop[next].replace(" |<imgsrc","<img src").replace(".png\">| ",".png\">").replace(" <br> ","<br>");
         return result;
     }
     else {
