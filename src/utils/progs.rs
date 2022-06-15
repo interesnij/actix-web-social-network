cc4a6fc463d749bdd73063e6d3bc0c03f4f4415d
interesnij <ochkarik1983@mail.ru>
2022-06-15 15:38:11 +0300

    J

--- src/utils/progs.rs
+++ src/utils/progs.rs
@@ -842,3 +842,214 @@ pub fn custom_link_check(value: &str) -> (i8, String) {
         return (1, "Занять адрес '".to_owned() + value + &"'".to_string());
     }
 }
+
+pub fn get_formatted_text(text: &str) -> String {
+    let zons: Vec<&str> = [
+            ".рф",".ru",".su",".net",".aero",".asia",".biz",".com",".info",".mobi",".name",".net",".org",".pro",".tel",
+            ".travel",".xxx",".ad", ".ae", ".af", ".ai",
+            ".al", ".am", ".aq", ".as', ".at", ".aw", ".ax", ".az", ".ba", ".be", ".bg", ".bh", ".bi", ".bj", ".bm",
+            ".bo", ".bs", ".bt", ".ca", ".cc", ".cd", ".cf", ".cg", ".ch", ".ci", ".cl", ".cm", ".cn", ".co",
+            ".cr", ".cu", ".cx", ".cz", ".de", ".dj", ". dk", ".dm", ".do", ".dz", ".ec", ".ee",
+            ".es", ".eu", ".fi", ".fo", ".fr", ".ga", ".gd", ".ge", ".gf", ".gg", ".gi", ".gl", ".gm", ".gp", ".gr",
+            ".gs", ".gy", " .hk", ".hm", ".hn", ".hr", ".ht", ".hu", ".ie", ".im", ".in", ".io", ".ir",
+            ".is", ".it", ".je", ".jo", ".jp", ".kg', ".ki", ".kn", ".kr", ".ky", ".kz", ".li", ".lk", ".lt",
+            ".lu", ".lv", ".ly", ".ma", ".mc", ".md", ".mg", ".mk", ".mo", ".mp", ".ms", ".mt", ".mu", ".mw",
+            ".mx", ".my", ".na", ".nc", ".nf", ".ng", ".nl", ".no", ".nu", ".nz",
+            ".pe", ".ph", ".pk", ".pl", ".pn", ".pr", ".ps", ".pt", ".re", ".ro", ". rs", ".rw", ".sd", ".se", ".sg",
+            ".si", ".sk", ".sl", ".sm", ".sn", ".so", ".sr", ".st", ".sz", ".tc", ".td", ".tg", ".tj", ".tk", ".tl",
+            ".tm", ".tn", ".to", ".tt", ".tw", ".ua", ".ug", ".uk", ".us", ".vc", ".vg", ".vn", ".vu", ".ws", ".academy",
+            ".accountant", ".accountants", ".actor", ".adult", ".africa", ".agency", ".airforce", ".apartments", ".app", ".army",
+            ".art", ".associates", ".attorney", ".auction", ".audio", ".auto", ".band", ".bank", ".bar", ".bargains", ".bayern", ".beer",
+            ".berlin", ".best", ".bet", ".bid", ".bike", ".bingo", ".bio", ".black", ".blackfriday", ".blog", ".blue", ".boutique",
+            ".broker", ".brussels", ".build", ".builders", ".business", ".buzz", ".cab", ".cafe", ".cam", ".camera", ".camp", ".capital",
+            ".car", ".cards", ".care", ".career", ".careers", ".cars", ".casa", ".cash", ".casino", ".cat", ".catering", ".center",
+            ".ceo", ".charity", ".chat", ".cheap", ".christmas", ".church", ".city", ".claims", ".cleaning", ".click", ".clinic",
+            ".clothing", ".cloud", ".club", ".coach", ".codes", ".coffee", ".college", ".cologne", ".community", ".company", ".computer",
+            ".condos", ".construction", ".consulting", ".contractors", ".cooking", ".cool", ".coop", ".country", ".coupons", ".courses",
+            ".credit", ".creditcard", ".cricket", ".cruises", ".dance", ".date", ".dating", ".deals", ".degree", ".delivery", ".democrat",
+            ".dental", ".dentist", ".desi", ".design", ".diamonds", ".diet", ".digital", ".direct", ".directory", ".discount", ".doctor",
+            ".dog", ".domains", ".download", ".earth", ".education", ".email", ".energy", ".engineer", ".engineering", ".enterprises",
+            ".equipment", ".estate", ".events", ".exchange", ".expert", ".exposed", ".express", ".fail", ".faith", ".family", ".fans",
+            ".farm", ".fashion", ".film", ".finance", ".financial", ".fish", ".fishing", ".fit", ".fitness", ".flights", ".florist",
+            ".flowers", ".fm", ".football", ".forex", ".forsale", ".foundation", ".fun", ".fund", ".furniture", ".futbol", ".fyi",
+            ".gallery", ".game", ".games", ".garden", ".gent", ".gift", ".gifts", ".gives", ".glass", ".global", ".gmbh", ".gold", ".golf",
+            ".graphics", ".gratis", ".green", ".gripe", ".group", ".guide", ".guitars", ".guru", ".haus", ".healthcare", ".help",
+            ".hiphop", ".hockey", "holdings", ".holiday", ".horse", ".hospital", ".host", ".hosting", ".house", ".how", ".immo",
+            ".immobilien", ".industries", ".ink", ".institute", ".insure", ".international", ".investments", ".irish", ".jewelry",
+            ".jobs", ".juegos", ".kaufen", ".kim", ".kitchen", ".kiwi", ".land", ".lawyer", ".lease", ".legal", ".life", ".lighting",
+            ".limited", ".limo", ".link", ".live", ".llc", ".loan", ".loans", ".lol", ".london", ".love", ".ltd", ".luxe", ".luxury",
+            ".maison", ".management", ".market", ".marketing", ".mba", ".media", ".memorial", ".men", ".menu", ".miami", ".moda", ".moe",
+            ".mom", ".money", ".mortgage", ".moscow", ".movie", ".navy", ".network", ".news", ".ninja", ".observer", ".one", ".onl",
+            ".online", ".ooo", ".page", ".paris", ".partners", ".parts", ".party", ".pet", ".photo", ".photography", ".photos",
+            ".pics", ".pictures", ".pink", ".pizza", ".plumbing", ".plus", ".poker", ".press", ".productions", ".promo",
+            ".properties", ".property", ".protection", ".pub", ".qpon", ".racing", ".radio", ".realty",
+            ".recipes", ".red", ".rehab", ".reisen", ".rent", ".rentals", ".repair", ".report", ".republican", ".rest", ".restaurant",
+            ".review", ".reviews", ".rich", ".rip", ".rocks", ".rodeo", ".run", ".sale", ".salon", ".sarl", ".school",
+            ".schule", ".science", ".security", ".services", ".sex", ".sexy", ".shiksha", ".shoes", ".shop", ".shopping",
+            ".show", ".singles", ".site", ".ski", ".soccer", ".social", ".software", ".solar", ".solutions", ".soy",
+            ".space", ".sport", ".store", ".stream", ".studio", ".study", ".style", ".sucks", ".supplies", ".supply",
+            ".support", ".surf", ".surgery", ".systems", ".tatar", ".tattoo", ".tax", ".taxi", ".team", ".tech",
+            ".technology", ".tennis", ".theater", ".theatre", ".tickets", ".tienda", ".tips", ".tires", ".tirol", ".today", ".tools",
+            ".top", ".tours", ".town", ".toys", ".trade", ".trading", ".training", ".tube", ".tv", ".university", ".uno", ".vacations",
+            ".vegas", ".ventures", ".vet", ".viajes", ".video", ".villas", ".vin", ".vip", ".vision", ".vodka", ".vote", ".voting",
+            ".voto", ".voyage", ".watch", ".webcam", ".website", ".wedding", ".wien", ".wiki", ".win", ".wine", ".work", ".works",
+            ".world", ".wtf", ".xyz", ".yoga", ".zone", ".дети", ".москва", ".онлайн", ".орг", ".рус", ".сайт"
+        ]
+    let _words = text
+        .replace("<img src"," |<imgsrc")
+        .replace(".png\">",".png'>| ")
+        .replace("<br>"," <br>")
+        .replace("  "," ");
+    let words: Vec<&str> = _words.split(" ").collect();
+    let words_count = words.len();
+    if words_count > 0 {
+        let mut _loop: Vec<&str> = Vec::new();
+        let mut _exlude: Vec<&str> = Vec::new();
+        let mut this: i8 = -1;
+        let mut next: i8 = 0;
+
+        _loop.push(text);
+        for word in words.iter().enumerate() {
+            let indent: &str;
+            if count == 0 && count == words_count {
+                indent = "";
+            }
+            else {
+                indent = " ";
+            }
+            if word == "".to_string():
+                continue;
+            }
+
+            if word.contains("#") {
+                _loop.push("".to_string());
+                this += 1;
+                next += 1;
+
+                //if word[0] == "#".to_string() {
+                _loop[next] = _loop[this].replace(indent + word, indent + "<a class='ajax action'href='/search/?q=%23'" + word + "'>" + word + "</a>", 1).replace("#<", "<");
+                //}
+            }
+            else if word[0] == "@" {
+                use crate::models::{CustomLink, User, Community};
+                use crate::schema::{
+                    custom_links::dsl::custom_links,
+                    users::dsl::users,
+                    communitys::dsl::communitys,
+                };
+
+                let _connection = establish_connection();
+                let mut exists = false;
+                let mut name = "".to_string();
+                let mut link = "".to_string();
+                if word[..3] == "@id" {
+                    let users_list = users
+                        .filter(schema::users::link.eq("/" + word[..1] + "/"))
+                        .load::<User>(&_connection)
+                        .expect("E.");
+                    if users_list.len() > 0 {
+                        exists = true;
+                        let user = users_list[0];
+                        name = user.get_full_name();
+                        link = user.link;
+                    }
+                }
+                else if word[..7] == "@public" {
+                    let communitys_list = communitys
+                        .filter(schema::communitys::link.eq("/" + word[..1] + "/"))
+                        .load::<Community>(&_connection)
+                        .expect("E.");
+                    if communitys_list.len() > 0 {
+                        exists = true;
+                        let community = communitys_list[0];
+                        name = community.name;
+                        link = community.link;
+                    }
+                }
+                else if custom_links
+                    .filter(schema::custom_links::link.eq(word[..1]))
+                    .load::<CustomLink>(&_connection)
+                    .expect("E.")
+                    .len() > 0 {
+                    let link = custom_links
+                        .filter(schema::custom_links::link.eq(word[..1]))
+                        .load::<CustomLink>(&_connection)
+                        .expect("E.")
+                        .into_iter()
+                        .nth(0)
+                        .unwrap();
+
+                    if link.owner == 1 {
+                        let users_list = users
+                            .filter(schema::users::link.eq(word[..1]))
+                            .load::<User>(&_connection)
+                            .expect("E.");
+                        if users_list.len() > 0 {
+                            exists = true;
+                            let user = users_list[0];
+                            name = user.get_full_name();
+                            link = user.link;
+                        }
+                    }
+                    else if communitys
+                        .filter(schema::communitys::link.eq(word[..1]))
+                        .load::<Community>(&_connection)
+                        .expect("E.")
+                        .len() > 0 {
+                            let communitys_list = communitys
+                                .filter(schema::communitys::link.eq("/" + word[..1] + "/"))
+                                .load::<Community>(&_connection)
+                                .expect("E.");
+                            if communitys_list.len() > 0 {
+                                exists = true;
+                                let community = communitys_list[0];
+                                name = community.name;
+                                link = community.link;
+                        }
+                    }
+                }
+                if exists {
+                    _loop.append("".to_string());
+                    this += 1;
+                    next += 1;
+                    _loop[next] = _loop[this].replace(indent + word, indent + &"<a class='action ajax show_mention_info pointer'href='/'".to_string() + &word[..0].to_string() + &"/'>".to_string() + &name + &"</a>".to_string(), 1);
+                }
+            }
+
+            else if word.contains(".") && !word.contains(".png") {
+                let mut p_2: &str;
+                if word[0] == "h" {
+                    p_2 = word;
+                }
+                else {
+                    p_2 = "//".to_owned() + &word;
+                }
+                if word.contains("трезвый.рус") {
+                    _loop.push("");
+                    p_2 = word.replace("трезвый.рус", "/").replace("http://", "").replace("https://", "");
+                    this += 1;
+                    next += 1;
+                    _loop[next] = _loop[this].replace(indent + &word, indent + &"<a class='ajax action'href='" + &p_2 + &"'>".to_string() + &word[..30].to_string() + &"</a>".to_string(), 1);
+                }
+                else {
+                    for zone in zons {
+                        if word.contains(zone) {
+                            let count = zone.len();
+                            if word[..count] == zone {
+                                _loop.push("");
+                                this += 1;
+                                next += 1;
+                                _loop[next] = _loop[this].replace(word, "<a class='action'rel='nofollow'target='_blank'href='".to_string() + &p_2 + &"'>".to_string() + &word[:30].to_string() + &"</a>".to_string(), 1);
+                            }
+                        }
+                    }
+                }
+            }
+            let result = _loop[next].replace(" |<imgsrc","<img src").replace(".png\">| ",".png\">").replace(" <br> ","<br>");
+            return result;
+        }
+        else {
+            return text;
+        }
+    }
+}
