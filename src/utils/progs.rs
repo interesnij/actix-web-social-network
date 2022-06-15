3983715362876b994efd34cfbb253e933ce1197c
interesnij <ochkarik1983@mail.ru>
2022-06-15 17:32:48 +0300

    J

--- src/utils/progs.rs
+++ src/utils/progs.rs
@@ -1012,7 +1012,7 @@ pub fn get_formatted_text(text: &str) -> String {
                     _loop.push("".to_string());
                     next += 1;
                     let rr_r = _loop[this];
-                    _loop[next] = rr_r.replace(indent.to_owned() + &word, &(indent.to_owned() + &"<a class='action ajax show_mention_info pointer'href='/'".to_string() + &word[..0].to_string() + &"/'>".to_string() + &name + &"</a>".to_string()));
+                    _loop[next] = rr_r.replace(&(indent.to_owned() + &word), &(indent.to_owned() + &"<a class='action ajax show_mention_info pointer'href='/'".to_string() + &word[..0].to_string() + &"/'>".to_string() + &name + &"</a>".to_string()));
                     this += 1;
                 }
             }
@@ -1029,7 +1029,7 @@ pub fn get_formatted_text(text: &str) -> String {
                     _loop.push("".to_string());
                     p_2 = &word.replace("трезвый.рус", "/").replace("http://", "").replace("https://", "");
                     next += 1;
-                    _loop[next] = _loop[this].replace(indent.to_owned() + &word, &(indent.to_owned() + &"<a class='ajax action'href='" + &p_2 + &"'>".to_string() + &word[..30].to_string() + &"</a>".to_string()));
+                    _loop[next] = _loop[this].replace(&(indent.to_owned() + &word), &(indent.to_owned() + &"<a class='ajax action'href='" + &p_2 + &"'>".to_string() + &word[..30].to_string() + &"</a>".to_string()));
                     this += 1;
                 }
                 else {
