f829953f396a519b6c010316f7206706661721ee
interesnij <ochkarik1983@mail.ru>
2022-06-17 12:23:36 +0300

    J

--- static/scripts/posts/user_post.js
+++ static/scripts/posts/user_post.js
@@ -843,7 +843,7 @@ on('#ajax', 'click', '#create_list_btn', function() {
         _class = "load_profile_good_list";
       }
       else if (folder == "/music") {
-        _svg = "<svg class='pointer load_profile_music_list svg_default list_toggle list_svg' width='50' height='50' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='1' stroke-linecap='round' stroke-linejoin='round'><g><rect fill='none' height='24' width='24'/><path d='M18,6h-2c0-2.21-1.79-4-4-4S8,3.79,8,6H6C4.9,6,4,6.9,4,8v12c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2V8C20,6.9,19.1,6,18,6z M12,4c1.1,0,2,0.9,2,2h-4C10,4.9,10.9,4,12,4z M18,20H6V8h2v2c0,0.55,0.45,1,1,1s1-0.45,1-1V8h4v2c0,0.55,0.45,1,1,1s1-0.45,1-1V8 h2V20z' /></g></svg>";
+        _svg = "<svg class='pointer load_profile_music_list svg_default list_toggle list_svg' width='50' height='50' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='1' stroke-linecap='round' stroke-linejoin='round'><path d='M9 18V5l12-2v13'></path><circle cx='6' cy='18' r='3'></circle><circle cx='18' cy='16' r='3'></circle></svg>";
         _class = "load_profile_music_list";
       }
       else if (folder == "/photos") {
